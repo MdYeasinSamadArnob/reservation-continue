d72c9355bb05d2b3097c184e00fcf01596e6eb0b
MdYeasinSamadArnob <yeasinsamadarnob@gmail.com>
2022-06-08 21:08:17 +0600

    5 files modified, 2 added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+// import '/node_modules/react-grid-layout/css/styles.css';
+// import '/node_modules/react-resizable/css/styles.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import { DatePicker } from 'antd';
+import Sidebar from '../components/share/SideBar';
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    // <h1 className="text-3xl font-bold underline">
+    //   Hello world! <DatePicker />
+    // </h1>
+    <Sidebar />
+  );
 }
